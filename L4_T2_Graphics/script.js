e8e514c24991646c065b9490fa219e8ca6475c66
Irina.Zelinko <zelirina23@gmail.com>
2021-11-23 18:03:07 +0300

    Fixed in file script.

--- L4_T2_Graphics/script.js
+++ L4_T2_Graphics/script.js
@@ -1,7 +1,11 @@
 window.onload = init; 
 
 function init(){
-    enlargeImage("rabbits", "img/cat.jpg");
+    enlargeImage("rabbits", "img/1558692302_2.jpeg");
+    enlargeImage("cat", "img/cat.jpg");
+    enlargeImage("lion", "img/lion.jpg");
+    enlargeImage("pair", "img/path.jpg");
+    enlargeImage("tiger", "img/tigers-face.jpg");
 }
 
 function enlargeImage(id, path){
@@ -9,9 +13,12 @@ function enlargeImage(id, path){
     console.dir(elem);
     if(elem.nodeName.toLowerCase() != "img")return;
 
-    let basePath = elem.src ;
+    let basePath = elem.src;
 
-    elem.addEventListener("click", function(){this.src = path;})
-    elem.addEventListener("click", function(){this.src = basePath;})
-
+    elem.addEventListener("click", function(){this.src = path;
+        this.style.width = 150 + "px";});
+    elem.addEventListener("mouseout", function(){
+        this.style.width = 80 + "px";
+        this.src = basePath;
+       });
 }
