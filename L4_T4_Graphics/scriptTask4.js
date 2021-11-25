28de4141a7b6d02dd10d1eb632138a343036cb6c
Irina.Zelinko <zelirina23@gmail.com>
2021-11-25 13:38:25 +0300

    Fixed  in script.

--- L4_T4_Graphics/scriptTask4.js
+++ L4_T4_Graphics/scriptTask4.js
@@ -1,5 +1,4 @@
-(function() {
-
+/* (function() {
 	var str = "";
 	var imgs = document.images;
 	document.getElementById("text").innerHTML = "text";
@@ -7,9 +6,7 @@
 	for (var i = 0; i < imgs.length; i++) {
 		str += imgs[i].src +"<br>";
 	}
-
 	document.getElementById("text").innerHTML = str;
-
 	var k = 0;
 	var id = setInterval(function() {
 		var j = k % imgs.length;
@@ -18,8 +15,34 @@
 		if (k == imgs.length) {
 			clearInterval(id);
 		}
-
 	}, 1000);
+}()); */
 
+window.onload = init;
+let imgs = document.images;
 
-}());
+function init() {
+	displayTheAddressesOfAllImages();
+	addARedFrameToEachImageInTurn();
+}
+
+function displayTheAddressesOfAllImages() {
+	let string = "";
+	document.getElementById("text").innerHTML = "text";
+	for (let i = 0; i < imgs.length; i++) {
+		string += imgs[i].src + "</br>";
+	}
+	document.getElementById("text").innerHTML = string;
+}
+
+function addARedFrameToEachImageInTurn() {
+	let number = 0;
+	let id = setInterval(function () {
+		let i = number % imgs.length;
+		imgs[i].style.border = " 3px solid red";
+		number++;
+		if (number == imgs.length) {
+			clearInterval(id);
+		}
+	}, 1000);
+}
