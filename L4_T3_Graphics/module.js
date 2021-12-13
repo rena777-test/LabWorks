21cfb276d0f574646691980f7c01fdc4e22b712a
Irina.Zelinko <zelirina23@gmail.com>
2021-12-13 19:25:30 +0300

    Update function.

--- L4_T3_Graphics/module.js
+++ L4_T3_Graphics/module.js
@@ -1,52 +1,3 @@
-/* window.addEventListener("load", init);
-
-  function init(){
-    let chart = createBarChart([30,8,88,10,5,45,100,150],
-        800,
-        300, 
-        "lightgreen",
-        "solid");
-        
-    let container = document.getElementById("chartContainer");
-    container.append(chart);
-    console.log(chart);
-    console.log(container);
-}
-
-function createBarChart(data, width, height, color, border){
-    let chart = document.createElement("div");
-  chart.style.width = width + "px";
-  chart.style.height = height + "px";
-  chart.style.position = "relative";
-  chart.style.border = border;
-
-  let max = Number.NEGATIVE_INFINITY;
-  for(let i = 0; i < data.length;i++){
-      if(max < data[i]){
-          max = data[i];
-      }
-  }
-  let scale = height/max;
-  let barWidth = Math.floor(width/ data.length);
-
-  for(let i = 0; i < data.length;i++){
-      let bar = document.createElement("div");
-
-      bar.style.height = data[i] * scale + "px";
-      bar.style.width = barWidth -4 + "px";
-
-      bar.style.position = "absolute";
-      bar.style.margin = "4px";
-      bar.style.bottom = "0px";
-      bar.style.left = barWidth * i + "px";
-
-      bar.style.backgroundColor = color;
-      chart.append(bar);
-  }
-  return chart;
-}  
- */
-
 window.addEventListener("load", init , false);
 
 function init(){
@@ -55,9 +6,7 @@ function init(){
         window.innerWidth -5,
         300,
         "lightblue");
-    
-    container.append(chart);
-    console.log(container);   
+    container.append(chart);  
 }
 
 function createBarChart(data,width,height,color){
@@ -89,8 +38,12 @@ function createBarChart(data,width,height,color){
 
         chart.append(bar);
     }
-        function onOver(){this.style.fill = "lightgray";}
-        function onOut(){this.style.fill = color;}
+        function onOver(){
+            this.style.fill = "lightgray";
+        }
+        function onOut(){
+            this.style.fill = color;
+        }
 
     return chart;
 }
