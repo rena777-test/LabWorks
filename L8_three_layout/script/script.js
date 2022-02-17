1a3376fc4cabdfcfaabbcbf68708cda15be373ce
Irina.Zelinko <zelirina23@gmail.com>
2022-02-17 19:18:35 +0300

    BT-fixed slider1 and slider2.

--- L8_three_layout/script/script.js
+++ L8_three_layout/script/script.js
@@ -22,52 +22,62 @@ closeBtn.addEventListener('click', function(){
 //выполняю подключение слайдера
 $('.poster-slider').slick(
   {
-    centerMode: true,
+    arrows:true,
+    accessibility:false,
     slidesToShow: 1,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          centerMode: true,
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          centerMode: true,
-          slidesToShow: 1
-        }
-      }
-    ]
+    slidesToScroll: 1,
+    centerMode:false,
+    variableWidth:false
   }
 );//задаю функцию доллар, внутри селектор кот меня интересует,вызываю метод slick
 $('.ours-slider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    responsive: [
+
+  adaptiveHeight:true ,  //чтобы заработало надо slick-track включить align-items:flex-start
+  slidesToShow:4,
+  slidesToScroll:1,
+  speed:1000,
+  easing:'ease',
+  initialSlide: 0,
+  infinite:true,
+  autoplay:true,
+  autoplaySpeed:1000,
+  pauseOnFocus:true,
+  pauseOnHover:true,
+  pauseOnDotsHover:true,
+  draggable:true, // для ПК //если это св-во, то верхние три не будут работать
+  swipe:true,// для телефона//
+  touchThreshold:5, 
+  touchMove:true, //при зажатой мышке не потянешь,а при true можно шевелить влево вправо
+  waitForAnimate:true,
+  centerMode:false,  // при добавлении появляется класс slick-center
+  variableWidth:true
+ /*    
+   responsive: [
         {
-          breakpoint: 992,
+          breakpoint: 1020,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 4,
             slidesToScroll: 1,
+            draggable:true
           }
         },
         {
           breakpoint: 768,
           settings: {
             slidesToShow: 2,
+            draggable:true
           }
         },
         {
           breakpoint: 576,
           settings: {
             slidesToShow: 1,
+            draggable:true
           }
         }
-       ] 
+       ]  */
 });
+
 
 /* new WOW({
   animateClass: 'animate__animated'
